3e519e6f9afb1ae209b11133937cafff745d2192
Barry Deeney <mxaddict@codedmaster.com>
2017-05-05 03:01:32 +0800

    This now gets the results for the default date range

--- main.js
+++ main.js
@@ -19,13 +19,38 @@ let crawler = new _crawler().configure({depth: 1});
 log("Instantiated crawler");
 log("Loading results page");
 
+// We need a place to save our results
+var _results = [];
+
 // Load the PSCO results page
 crawler.crawl({
   url: "http://www.pcso.gov.ph/lotto-search/lotto-search.aspx",
   success: function(page) {
+    // Page loaded, nice
     log("Results page loaded");
-    let dom = new JSDOM(page.body);
-    console.log(dom);
+
+    // Parse the document
+    log("Parse the document");
+    let document = (new JSDOM(page.body)).window.document;
+
+    // Load the results
+    log("Parse the result rows");
+    let results = document.querySelectorAll('#GridView1 tr td');
+    let results_count = results.length / 5;
+
+    // Loop the results
+    for (var i = 0, len = results_count; i < len; i++) {
+      var x = i * 5;
+      _results.push({
+        game:    results[x+0].innerHTML,
+        result:  results[x+1].innerHTML,
+        date:    results[x+2].innerHTML,
+        prize:   results[x+3].innerHTML,
+        winners: results[x+4].innerHTML
+      });
+    }
+
+    log(_results);
   },
   failure: function(page) {
     log('ERROR: ' + page.status + ' | Could not load page');
