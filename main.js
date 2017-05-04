1370dadc135757ac68b4c1359ecd388d04bbd2a9
Barry Deeney <mxaddict@codedmaster.com>
2017-05-05 02:03:27 +0800

    Added figlet and js-crawler packages

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-// We need an easy way to use the console.log command
-let log = console.log;
+// Load dependencies
+let figlet  = require('figlet');
+let crawler =
 
-log("Started the scraper");
+// We need an easy way to use the console.log command
+let log    = console.log;
+let fig    = (string) =>
+  log(figlet.textSync(string.replace(/ /g, '  ')))
+
+fig("Started the scraper");
